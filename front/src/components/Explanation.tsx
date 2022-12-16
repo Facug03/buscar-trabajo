fb697e698e86b4070b9d4e3cd7de8d050dc89499
Facug03 <facundogonzales1550@gmail.com>
2022-12-15 22:17:37 -0300

    emoji added

--- front/src/components/Explanation.tsx
+++ front/src/components/Explanation.tsx
@@ -10,14 +10,14 @@ export default function Explanation() {
         onClick={() => setInfo(!info)}
       >
         <h2 className='text-3xl font-lexendBold text-white max-[355px]:text-2xl'>
-          + ¿Cómo funciona?
+          <span>{info ? '-' : '+'}</span> ¿Cómo funciona?
         </h2>
       </div>
       {info && (
         <div className='bg-[#eeeef0] px-3 py-4 rounded'>
           <div className='mb-4'>
             <h3 className='mb-2 text-base text-primary font-lexendBold'>
-              El primer paso es agregar un link a un portal de empleo de una
+              ▶ El primer paso es agregar un link a un portal de empleo de una
               empresa. Podés agregar cuantos links quieras:
             </h3>
             <p className='mb-2 pl-2'>Ejemplo:</p>
@@ -25,7 +25,7 @@ export default function Explanation() {
               <div className='relative grid gap-3 p-4 grid-cols-5 max-ph:grid-cols-1 items-center rounded-md border-black border-2 whitespace-nowrap'>
                 <div>Nombre</div>
                 <div className='col-span-4 max-ph:hidden'>Link</div>
-                <button className='absolute right-[15px] top-[11px] flex items-center gap-1 bg-primary py-1 px-2 text-white rounded-md hover:bg-secondary duration-200 ease-linear'>
+                <span className='absolute right-[15px] top-[11px] flex items-center gap-1 bg-primary py-1 px-2 text-white rounded-md'>
                   Añadir
                   <svg
                     stroke='#ffffff'
@@ -37,7 +37,7 @@ export default function Explanation() {
                   >
                     <polygon points='42,20 22,20 22,0 20,0 20,20 0,20 0,22 20,22 20,42 22,42 22,22 42,22 '></polygon>
                   </svg>
-                </button>
+                </span>
                 <div className='cursor-pointer overflow-hidden overflow-ellipsis'>
                   Eudamonia
                 </div>
@@ -48,7 +48,7 @@ export default function Explanation() {
             </div>
           </div>
           <h3 className='mb-2 text-base text-primary font-lexendBold'>
-            El segundo paso es elegir la palabra clave:
+            ▶ El segundo paso es elegir la palabra clave:
           </h3>
           <p className='mb-2 pl-2'>Ejemplo:</p>
           <div className='bg-white p-2 rounded mb-4'>
@@ -73,7 +73,7 @@ export default function Explanation() {
             </div>
           </div>
           <h3 className='mb-2 text-base text-primary font-lexendBold'>
-            El tercer paso es darle al boton de check y aparecerán todos los
+            ▶ El tercer paso es darle al boton de check y aparecerán todos los
             resultados:
           </h3>
           <p className='mb-2 pl-2'>Ejemplo:</p>
